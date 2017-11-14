1d11d29836af1ee932a06d1b4de542f6796babb7
lushantingyue <diyikehan@126.com>
2017-11-14 14:29:51 +0800

    tab导航栏 1px border边框的实现, mixin样式预处理

--- src/App.vue
+++ src/App.vue
@@ -30,12 +30,15 @@
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
+  @import "./common/stylus/mixin.styl"
+
   #app
     .tab
       display: flex
       width: 100%
       height: 40px
       line-height: 40px
+      border-1px(rgba(7, 17, 27, 0.1))
       .tab-item
         flex: 1
         text-align: center
