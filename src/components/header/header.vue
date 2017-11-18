43c2e073097ca49ceb6394c22bff13aeb7d47761
lushantingyue <diyikehan@126.com>
2017-11-19 01:15:36 +0800

    弹出层商家名称

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -39,7 +39,7 @@
     <div v-show="detailShow" class='detail'>
       <div class="detail-wrapper clearfix" >
         <div class="detail-main">
-          
+          <h1 class="name">{{ seller.name }}</h1>
         </div>
       </div>
       <div class="detail-close">
@@ -208,9 +208,16 @@
       background : rgba(7, 17, 27, 0.8)
       .detail-wrapper
         min-height : 100%
+        width : 100%;
         .detail-main
           margin-top : 64px
           padding-bottom : 64px
+          .name
+            line-height : 16px
+            text-align : center
+            font-size : 16px
+            font-weight : 700
+
       .detail-close
         position : relative
         width:32px
