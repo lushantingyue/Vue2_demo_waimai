3e23757c92e47b054d77faab754657f924ca5ad0
lushantingyue <diyikehan@126.com>
2017-11-18 17:59:31 +0800

    详情弹出层: @click=“” 点击事件绑定方法 showDetail()，v-show=“”绑定data值，弹出层的Css样式实现

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -20,14 +20,14 @@
         </div>
       </div>
 
-      <div v-if="seller.supports" class="support-count">
+      <div v-if="seller.supports" class="support-count" @click="showDetail">
         <span class="count">{{ seller.supports.length }}个</span>
         <i class="icon-keyboard_arrow_right"></i>
       </div>
 
     </div>
 
-    <div class="bulletin-wrapper">
+    <div class="bulletin-wrapper" @click="showDetail">
       <span class="bulletin-title"></span><span class="bulletin-text">{{ seller.bulletin }}</span>
       <i class="icon-keyboard_arrow_right"></i>
     </div>
@@ -35,6 +35,9 @@
     <div class="background">
       <img :src="seller.avatar" width="100%" height="100%"/>
     </div>
+
+    <div v-show="detailShow" class='detail'></div>
+
   </div>
 </template>
 
@@ -42,6 +45,16 @@
   export default {
     props: {
       seller: {type: Object}
+    },
+    data () {
+      return {
+        detailShow: false
+      }
+    },
+    methods: {
+      showDetail () {
+        this.detailShow = true
+      }
     },
     created () {
       this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee']
@@ -166,7 +179,6 @@
         right: 12px
         top: 3px
 
-
     .background
       position : absolute
       top: 0
@@ -175,5 +187,15 @@
       height: 100%
       z-index: -1
       filter : blur(10px)
+
+    .detail
+      position : fixed
+      z-index : 100
+      top : 0
+      left : 0
+      width :100%
+      height : 100%
+      overflow : auto
+      background : rgba(7, 17, 27, 0.8)
 
 </style>
