c936524692f2d7940afbe819ce2caa5af102101a
lushantingyue <diyikehan@126.com>
2017-11-22 17:38:30 +0800

    商品列表

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -1,6 +1,7 @@
 <template>
-  <div>
-    I'm goods.
+  <div class="good">
+    <div class="menu-wrapper"></div>
+    <div class="foods-wrapper"></div>
   </div>
 </template>
 
@@ -11,5 +12,19 @@
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
+
+  .good
+    display: flex
+    position: absolute
+    top: 186px
+    bottom: 46px
+    width: 100%
+    overflow: hidden
+    .menu-wrapper
+      flex: 0 0 80px
+      width: 80px
+      background: #f3f5f7
+    .foods-wrapper
+      flex: 1
 
 </style>
