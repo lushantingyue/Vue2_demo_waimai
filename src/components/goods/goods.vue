5e05135f808749bfe026b8f9102b73eeb4cb6884
lushantingyue <diyikehan@126.com>
2017-11-24 17:47:38 +0800

    实现商品信息列表

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -2,15 +2,40 @@
   <div class="good">
     <div class="menu-wrapper">
       <ul class="list">
-        <li v-for="item in goods" class="menu-item">
-          <span class="text" >
+        <li v-for="item in goods" class="menu-item" @click="showIndex(item)">
+          <span class="text">
             <span v-show="item.type>0" class="icon" :class="classMap[item.type]"></span>
             {{ item.name }}
           </span>
         </li>
       </ul>
     </div>
-    <div class="foods-wrapper"></div>
+    <div class="foods-wrapper">
+      <ul>
+        <li v-for="item in goods" class="food-list">
+          <h4 class="title">{{ item.name }}</h4>
+          <ul>
+            <li v-for="food in item.foods" class="food-item border-1px">
+              <div class="icon">
+                <img width="57" height="57" :src="food.image">
+              </div>
+              <div class="content">
+                <h2 class="name">{{food.name}}</h2>
+                <p class="desc">{{food.description}}</p>
+                <div class="extra">
+                  <span>月售{{food.sellCount}}份</span>
+                  <span>好评率{{food.rating}}%</span>
+                </div>
+                <div class="price">
+                  <span>￥{{food.price}}</span>
+                  <span v-show="food.oldPrice">￥{{food.oldPrice}}</span>
+                </div>
+              </div>
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -19,11 +44,12 @@
 
   export default {
     props: {
-      seller: { type: Object }
+      seller: {type: Object}
     },
     data () {
       return {
-        goods: []
+        goods: [],
+        listData: []
       }
     },
     created () {
@@ -35,6 +61,12 @@
           console.log(this.goods)
         }
       })
+    },
+    methods: {
+      showIndex (item) {
+//        this.listData = item.foods
+//        console.log(this.listData)
+      }
     }
   }
 </script>
@@ -47,6 +79,7 @@
     position: absolute
     top: 186px
     bottom: 46px
+    left: 0
     width: 100%
     overflow: hidden
     .menu-wrapper
@@ -54,7 +87,10 @@
       width: 80px
       background: #f3f5f7
       .list
-        margin: 0 -40px
+        font-size: 0
+        list-style-type: none
+        margin 0
+        padding 0
         .menu-item
           display: table
           height: 54px
@@ -89,5 +125,43 @@
 
     .foods-wrapper
       flex: 1
+      overflow: auto
+      .food-list
+        list-style-type: none
+        margin 0
+        padding 0
+        .title
+          padding-left: 14px
+          height: 26px
+          line-height: 26px
+          border-left: 2px solid #d9dde1
+          font-size: 12px
+          color: rgb(147, 153, 159)
+          background: #f3f5f7
+          .food-item
+            display: flex
+            margin: 18px
+            border-1px(rgba(7, 17, 27, 0.1))
+            .icon
+              flex 0 0 57px
+              margin-right: 10px
+            .content
+                flex: 1
+              .name
+                margin-top: 2px
+                font-size 14px
+                color: rgb(7, 17, 27)
+                height: 14px
+                line-height: 14px
+              .desc
+                font-size: 10px
+              .extra
+                margin: 8px 0
+                font-size: 10px
+              .price
+                font-size: 10px
 
+  //.ul
+  //list-style: none
+  //float: left
 </style>
