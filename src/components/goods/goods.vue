104cd234e96d3f133260d272502489dcf7a859f4
lushantingyue <diyikehan@126.com>
2017-11-27 16:54:42 +0800

    完善商品列表样式, 商品条目last-child元素下边框的隐藏

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -24,11 +24,11 @@
                 <p class="desc">{{food.description}}</p>
                 <div class="extra">
                   <span>月售{{food.sellCount}}份</span>
-                  <span>好评率{{food.rating}}%</span>
+                  <span class="count">好评率{{food.rating}}%</span>
                 </div>
                 <div class="price">
-                  <span>￥{{food.price}}</span>
-                  <span v-show="food.oldPrice">￥{{food.oldPrice}}</span>
+                  <span class="now">￥{{food.price}}</span>
+                  <span class="old" v-show="food.oldPrice">￥{{food.oldPrice}}</span>
                 </div>
               </div>
             </li>
@@ -136,7 +136,11 @@
           .food-item
             display: flex
             margin: 18px
+            padding-bottom: 18px
             border-1px(rgba(7, 17, 27, 0.1))
+            &:last-child
+              border-none()
+              margin-bottom: 0
             .icon
               flex 0 0 57px
               margin-right: 10px
@@ -144,17 +148,30 @@
                 flex: 1
               .name
                 margin-top: 2px
-                font-size 14px
-                color: rgb(7, 17, 27)
                 height: 14px
                 line-height: 14px
+                font-size 14px
+                color: rgb(7, 17, 27)
+              .desc, .extra
+                line-height: 10px
+                font-size: 10px
+                color: rgb(147, 153, 159)
               .desc
-                font-size: 10px
+                margin-bottom: 8px
               .extra
-                margin: 8px 0
-                font-size: 10px
+                &.count
+                  margin-right: 12px
               .price
-                font-size: 10px
+                font-weight: 700
+                line-height: 24px
+                .now
+                  margin-right: 8px
+                  font-size: 14px
+                  color: rgb(240, 20, 20)
+                .old
+                  text-decoration: line-through
+                  font-size: 10px
+                  color: rgb(147, 153, 159)
 
   //.ul
   //list-style: none
