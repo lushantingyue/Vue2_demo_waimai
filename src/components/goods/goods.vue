85f2fad897515d9be6ae593f8d8e1230b3e00b0b
lushantingyue <diyikehan@126.com>
2017-12-04 17:47:25 +0800

    新增 "商品添加"组件

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -29,6 +29,9 @@
                   <span class="now">￥{{food.price}}</span>
                   <span class="old" v-show="food.oldPrice">￥{{food.oldPrice}}</span>
                 </div>
+                <div class="cartcontrol-wrapper">
+                  <cartcontrol :food="food"></cartcontrol>
+                </div>
               </div>
             </li>
           </ul>
@@ -42,6 +45,7 @@
 <script type="text/ecmascript-6">
   import BScroll from 'better-scroll'
   import shopcart from '../shopcart/shopcart'
+  import cartcontrol from '../cartcontrol/cartcontrol'
 
   const STATE_SUCESS = 0
 
@@ -115,7 +119,8 @@
       }
     },
     components: {
-      shopcart
+      shopcart,
+      cartcontrol
     }
   }
 </script>
