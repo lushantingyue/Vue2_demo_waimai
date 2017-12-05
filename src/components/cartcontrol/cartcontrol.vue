1f002124ea8ff99733e387121aeb8848111d35ef
lushantingyue <diyikehan@126.com>
2017-12-05 15:07:35 +0800

    给按钮添加 "滚动展开" 动画效果

--- src/components/cartcontrol/cartcontrol.vue
+++ src/components/cartcontrol/cartcontrol.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="cartcontrol">
-    <div class="cart-decrease icon-remove_circle_outline" @click="decreaseCart" v-show="food.count>0"></div>
+    <transition name="move">
+      <div class="cart-decrease" @click="decreaseCart" v-show="food.count>0">
+        <span class="inner icon-remove_circle_outline"></span>
+      </div>
+    </transition>
     <div class="cart-count" v-show="food.count>0">{{food.count}}</div>
     <div class="cart-increase icon-add_circle" @click="addCart"></div>
   </div>
@@ -10,13 +14,13 @@
   import Vue from 'vue'
 
   export default {
-
     props: {
       food: {
         type: Object
       }
     },
-    created () {},
+    created () {
+    },
     methods: {
       addCart (event) {
         if (!event._constructed) {
@@ -44,21 +48,39 @@
 <style lang="stylus" rel="stylesheet/stylus">
   .cartcontrol
     font-size: 0px
-    .cart-decrease, .cart-increase
+    .cart-decrease
       display: inline-block
       padding: 6px
-      font-size 24px
-      line-height: 24px
-      color: rgb(0, 160, 220)
+      transition: all 0.4s linear
+      &.move-transition
+        opacity: 1
+        transform: translate3d(0, 0, 0)
+      .inner
+        display: inline-block
+        line-height: 24px
+        font-size 24px
+        color: rgb(0, 160, 220)
+        transition: all 0.4s
+        transform: rotate(0)
+      &.move-enter, &.move-leave
+        opacity: 0
+        transform: translate3d(24px, 0, 0)
+        .inner
+          transform: rotate(180deg)
+
     .cart-count
       display: inline-block
       width: 12px
       vertical-align: top
-      padding-top : 6px
+      padding-top: 6px
       text-align: center
       font-size: 10px
       color: rgb(147, 153, 159)
     .cart-increase
       display: inline-block
+      padding: 6px
+      font-size 24px
+      line-height: 24px
+      color: rgb(0, 160, 220)
 
 </style>
