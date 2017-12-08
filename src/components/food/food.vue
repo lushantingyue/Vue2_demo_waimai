0ce5c0c26600a86b7ae9148be2cb80dacbbdebb1
lushantingyue <diyikehan@126.com>
2017-12-08 17:40:20 +0800

    padding-top: 100%属性, 实现高度自动计算

--- src/components/food/food.vue
+++ src/components/food/food.vue
@@ -69,5 +69,22 @@
     &.move-enter, &.move-leave-to
       transform translate3d(100%, 0, 0)
 
+    .food-content
+      position relative
+      display inline
+      font-size 0
+      .image-header
+        position: relative
+        width 100%
+        height 0
+        padding-top 100%
+        .img
+          position: absolute
+          top  0
+          left 0
+          width 100%
+          height 100%
+
+
 
 </style>
