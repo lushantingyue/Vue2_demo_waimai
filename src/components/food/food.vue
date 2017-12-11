d30a4ae7c85cccf822fcc256f26d59ce76391892
lushantingyue <diyikehan@126.com>
2017-12-11 21:48:10 +0800

    商品信息样式

--- src/components/food/food.vue
+++ src/components/food/food.vue
@@ -24,8 +24,11 @@
           </transition>
         </div>
         <split v-show="food.info"></split>
+        <div class="info" v-show="food.info">
+          <h1 class="title">商品信息</h1>
+          <p class="text">{{food.info}}</p>
+        </div>
       </div>
-      <div class="info"></div>
       <div class="rating"></div>
     </div>
   </transition>
@@ -150,27 +153,39 @@
           text-decoration: line-through
           font-size: 10px
           color: rgb(147, 153, 159)
-    .cartcontroll-wrapper
-      position: absolute
-      right 12px
-      bottom 12px
-    .buy
-      position: absolute
-      right: 18px
-      bottom: 18px
-      z-index: 10
-      height: 24px
-      line-height: 24px
-      padding: 0 12px
-      box-sizing: border-box
-      border-radius: 12px
-      font-size: 10px
-      color: #fff
-      background: rgb(0, 160, 220)
-      opacity 1
-      &.fade-enter-active, &.fade-leave-active
-        transition: all 0.2s
-      &.fade-enter, &.fade-leave-to
-        opacity: 0
+      .cartcontroll-wrapper
+        position: absolute
+        right 12px
+        bottom 12px
+      .buy
+        position: absolute
+        right: 18px
+        bottom: 18px
+        z-index: 10
+        height: 24px
+        line-height: 24px
+        padding: 0 12px
+        box-sizing: border-box
+        border-radius: 12px
+        font-size: 10px
+        color: #fff
+        background: rgb(0, 160, 220)
+        opacity 1
+        &.fade-enter-active, &.fade-leave-active
+          transition: all 0.2s
+        &.fade-enter, &.fade-leave-to
+          opacity: 0
 
+    .info
+      padding 18px
+      .title
+        line-height 14px
+        margin-bottom 6px
+        font-size 14px
+        color rgb(7, 17, 27)
+      .text
+        line-height 24px
+        padding 0 8px
+        font-size 12px
+        color rgb(77, 85, 93)
 </style>
