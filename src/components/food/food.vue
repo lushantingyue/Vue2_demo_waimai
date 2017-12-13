8d6bb5350ec156e1137f5b145b61ca112f811d9d
lushantingyue <diyikehan@126.com>
2017-12-13 15:24:42 +0800

    评价列表布局/样式/遍历绑定数据

--- src/components/food/food.vue
+++ src/components/food/food.vue
@@ -32,6 +32,21 @@
         <div class="rating">
           <h1 class="title">商品评价</h1>
           <ratingselect :selectType="selectType" :onlyContent="onlyContent" :ratings="food.ratings" :desc="desc"></ratingselect>
+          <div class="rating-wrapper">
+            <ul v-show="food.ratings && food.ratings.length">
+              <li v-for="rating in food.ratings" class="rating-item">
+                <div class="user">
+                  <span class="name">{{rating.username}}</span>
+                  <img :src="rating.avatar" class="avatar" width="12" height="12"/>
+                </div>
+                <div class="time">{{rating.rateTime}}</div>
+                <p class="text"><span :class="{'icon-thumb_up': rating.rateType===0, 'icon-thumb_down': rating.rateType===1}"></span>
+                  {{rating.text}}
+                </p>
+              </li>
+            </ul>
+            <div class="no-rating" v-show="!food.ratings || !food.ratings.length"></div>
+          </div>
         </div>
       </div>
     </div>
@@ -212,4 +227,7 @@
         margin-left 18px
         font-size 14px
         color rgb(7, 17, 27)
+      /*.rating-wrapper*/
+        /*.rating-item ul*/
+        /*.no-rating*/
 </style>
