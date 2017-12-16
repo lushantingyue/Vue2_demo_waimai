1cd9d831b95e709f1bb98a251741892c519a4973
lushantingyue <diyikehan@126.com>
2017-12-16 20:11:29 +0800

    update

--- src/components/seller/seller.vue
+++ src/components/seller/seller.vue
@@ -1,31 +1,127 @@
 <template>
-  <div>
-    I'm seller.
+  <div class="seller" ref="seller">
+    <div class="seller-content">
+      <div class="overview"></div>
+
+      <split></split>
+
+      <div class="bulletin">
+        <h1 class="title">公告与活动</h1>
+        <div class="content-wrapper">
+          <p class="content">{{seller.bulletin}}</p>
+        </div>
+        <ul class="supports" v-if="seller.supports">
+          <li class="support-item" v-for="(item, index) in seller.supports">
+            <span class="icon" :class="classMap[item.type]"></span>
+            <span class="text">{{item.description}}</span>
+          </li>
+        </ul>
+      </div>
+
+      <split></split>
+
+      <div class="pics">
+        <h1 class="title">商家实景</h1>
+        <span class="pic" v-for="pic in seller.pics" v-show="seller.pics">
+        <img :src="pic" width="240" height="180"/>
+      </span>
+      </div>
+
+      <split></split>
+
+      <div class="infos">
+        <h1 class="title">商家信息</h1>
+        <div class="text" v-for="text in seller.infos">{{text}}</div>
+      </div>
+    </div>
   </div>
+
 </template>
 
 <script type="text/ecmascript-6">
-  const STATE_SUCESS = 0
+  import split from '../../components/split/split'
 
   export default {
-    props: [],
-    data () {
-      return {
-        seller: Object
+    props: {
+      seller: {
+        type: Object
       }
     },
     created () {
-      this.$http.get('/api/seller').then((response) => {
-        response = response.body
-        if (response.errno === STATE_SUCESS) {
-          this.seller = response.data
-          console.log('=========' + this.seller)
-        }
-      })
+      this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee']
+    },
+    components: {
+      split
     }
   }
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
+  @import "../../common/stylus/mixin.styl"
+
+  .seller
+    position  absolute
+    .bulletin
+      padding 18px
+      .title
+        padding-bottom 8px
+        line-height 14px
+        font-size 14px
+        color rgb(7,17,27)
+      .content-wrapper
+        border-1px(rgba(7, 17, 27, 0.1))
+        .content
+          line-height 24px
+          font-size 12px
+          color rgb(240, 20, 20)
+          padding 0 12px 16px 12px
+      .supports
+        .support-item
+          padding 16px 12px
+          border-1px(rgba(7, 17, 27, 0.1))
+          font-size 0px
+          .text
+            line-height 16px
+            font-size 12px
+            color rgb(7,17,27)
+          /*.icon*/
+            /*display: inline-block*/
+            /*width: 12px*/
+            /*height: 12px*/
+            /*margin-right: 4px*/
+            /*background-size: 12px, 12px*/
+            /*background-repeat: no-repeat*/
+            /*&.decrease*/
+              /*bg-image('decrease_1')*/
+            /*&.discount*/
+              /*bg-image('discount_1')*/
+            /*&.guarantee*/
+              /*bg-image('guarantee_1')*/
+            /*&.invoice*/
+              /*bg-image('invoice_1')*/
+            /*&.special*/
+              /*bg-image('special_1')*/
+    .pics
+      padding 18px 0 18px 18px
+      .title
+        padding-bottom 12px
+        line-height 14px
+        font-size 14px
+        color rgb(7,17,27)
+    .infos
+      padding 18px
+      .title
+        padding-bottom 12px
+        line-height 14px
+        font-size 14px
+        color rgb(7,17,27)
+        border-1px(rgba(7, 17, 27, 0.1))
+      .text
+        padding 16px 12px
+        line-height 16px
+        font-size 12px
+        color rgb(7,17,27)
+        border-1px(rgba(7, 17, 27, 0.1))
+
 
 </style>
