a2171832817238f425940ce34589798cd6717bd3
lushantingyue <diyikehan@126.com>
2017-12-16 20:45:38 +0800

    update

--- src/components/seller/seller.vue
+++ src/components/seller/seller.vue
@@ -1,7 +1,14 @@
 <template>
   <div class="seller" ref="seller">
     <div class="seller-content">
-      <div class="overview"></div>
+      <div class="overview">
+        <h1 class="title">{{seller.name}}</h1>
+        <div class="desc border-1px">
+          <star :size="36" :score="seller.score"></star>
+          <span class="text">({{seller.ratingCount}})</span>
+          <span class="text" v-if="seller.sellCount">月售{{seller.sellCount}}单</span>
+        </div>
+      </div>
 
       <split></split>
 
@@ -40,6 +47,7 @@
 
 <script type="text/ecmascript-6">
   import split from '../../components/split/split'
+  import star from '../../components/star/star'
 
   export default {
     props: {
@@ -51,7 +59,8 @@
       this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee']
     },
     components: {
-      split
+      split,
+      star
     }
   }
 </script>
@@ -60,7 +69,30 @@
   @import "../../common/stylus/mixin.styl"
 
   .seller
-    position  absolute
+    position absolute
+    .overview
+      position relative
+      padding 18px
+      .title
+        margin-bottom 8px
+        line-height 14px
+        font-size 14px
+        color rgb(7,17,27)
+      .desc
+        padding-bottom 18px
+        border-1px(rgba(7, 17, 27, 0.1))
+        font-size 0
+        .star
+          display inline-block
+          padding-right 8px
+          vertical-align top
+        .text
+          display inline-block
+          margin-right 12px
+          vertical-align top
+          line-height 18px
+          font-size 10px
+          color rgb(77,85,93)
     .bulletin
       padding 18px
       .title
@@ -80,27 +112,31 @@
           padding 16px 12px
           border-1px(rgba(7, 17, 27, 0.1))
           font-size 0px
+          &:last-child
+            border-none()
           .text
             line-height 16px
             font-size 12px
             color rgb(7,17,27)
-          /*.icon*/
-            /*display: inline-block*/
-            /*width: 12px*/
-            /*height: 12px*/
-            /*margin-right: 4px*/
-            /*background-size: 12px, 12px*/
-            /*background-repeat: no-repeat*/
-            /*&.decrease*/
-              /*bg-image('decrease_1')*/
-            /*&.discount*/
-              /*bg-image('discount_1')*/
-            /*&.guarantee*/
-              /*bg-image('guarantee_1')*/
-            /*&.invoice*/
-              /*bg-image('invoice_1')*/
-            /*&.special*/
-              /*bg-image('special_1')*/
+          .icon
+            display: inline-block
+            vertical-align top
+            width: 16px
+            height: 16px
+            margin-right: 6px
+            background-size: 16px, 16px
+            background-repeat: no-repeat
+            &.decrease
+              bg-image('decrease_2')
+            &.discount
+              bg-image('discount_2')
+            &.guarantee
+              bg-image('guarantee_2')
+            &.invoice
+              bg-image('invoice_2')
+            &.special
+              bg-image('special_2')
+
     .pics
       padding 18px 0 18px 18px
       .title
