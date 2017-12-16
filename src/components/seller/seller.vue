8204a7df23143c53616f86113e275f5a41af8e5a
lushantingyue <diyikehan@126.com>
2017-12-16 16:07:09 +0800

    ajax请求seller接口

--- src/components/seller/seller.vue
+++ src/components/seller/seller.vue
@@ -5,8 +5,24 @@
 </template>
 
 <script type="text/ecmascript-6">
+  const STATE_SUCESS = 0
+
   export default {
-    props: []
+    props: [],
+    data () {
+      return {
+        seller: Object
+      }
+    },
+    created () {
+      this.$http.get('/api/seller').then((response) => {
+        response = response.body
+        if (response.errno === STATE_SUCESS) {
+          this.seller = response.data
+          console.log('=========' + this.seller)
+        }
+      })
+    }
   }
 </script>
 
