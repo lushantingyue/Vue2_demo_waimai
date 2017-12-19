08839ab810d134547384f2b396c3e344e96d92da
lushantingyue <diyikehan@126.com>
2017-12-19 13:19:57 +0800

    实现商家页Overview部分

--- src/components/seller/seller.vue
+++ src/components/seller/seller.vue
@@ -8,6 +8,26 @@
           <span class="text">({{seller.ratingCount}})</span>
           <span class="text" v-if="seller.sellCount">月售{{seller.sellCount}}单</span>
         </div>
+        <ul class="remark">
+          <li class="block">
+            <h2>起送价</h2>
+            <div class="content">
+              <span class="stress">{{seller.minPrice}}</span>元
+            </div>
+          </li>
+          <li class="block">
+            <h2>商家配送</h2>
+            <div class="content">
+              <span class="stress">{{seller.deliveryPrice}}</span>元
+            </div>
+          </li>
+          <li class="block">
+            <h2>平均配送时间</h2>
+            <div class="content">
+              <span class="stress">{{seller.deliveryTime}}</span>分钟
+            </div>
+          </li>
+        </ul>
       </div>
 
       <split></split>
@@ -70,6 +90,11 @@
 
   .seller
     position absolute
+    top: 174px
+    bottom: 46px
+    left: 0
+    width: 100%
+    overflow: hidden
     .overview
       position relative
       padding 18px
@@ -93,6 +118,26 @@
           line-height 18px
           font-size 10px
           color rgb(77,85,93)
+      .remark
+        display flex
+        padding-top 18px
+        .block
+          flex 1
+          text-align center
+          border-right 1px solid rgba(7, 17, 27, 0.1)
+          &:last-child
+            border-right none
+          h2
+            margin-bottom 4px
+            line-height 10px
+            font-size 10px
+            color rgb(147, 153, 159)
+          .content
+            line-height 24px
+            font-size 10px
+            color rgb(7, 17, 27)
+            .stress
+              font-size 24px
     .bulletin
       padding 18px
       .title
@@ -101,12 +146,12 @@
         font-size 14px
         color rgb(7,17,27)
       .content-wrapper
+        padding 0 12px 16px 12px
         border-1px(rgba(7, 17, 27, 0.1))
         .content
           line-height 24px
           font-size 12px
           color rgb(240, 20, 20)
-          padding 0 12px 16px 12px
       .supports
         .support-item
           padding 16px 12px
